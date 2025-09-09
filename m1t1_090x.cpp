356a6f1e4831b7a6d56a84bafad2932dc44fdfa0
moodye4980 <160150623+moodye4980@users.noreply.github.com>
2025-09-09 16:17:31 +0000

    finished up, didn't take long

--- m1t1_090x.cpp
+++ m1t1_090x.cpp
@@ -4,5 +4,26 @@ M1T1 for the online sections
 Emma Moody
 9/9/25
 Program will greet the user
+
+ERROR HANDING WITH CODE SPACES:
+IF it happens:
+"stage changes" , no, push directly
+"commit and sync" , always
+"pull?" doesn't matter
 */
 
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+    // input: start the program
+    // processing: none
+    // output: 0 (no errors)
+    // special FX: Says "Hello" to the user.
+
+    // Let's say hello :)
+    cout << "Hello, user!" << endl; // "end line" not "end 1"
+
+    return 0; // no errors
+}
