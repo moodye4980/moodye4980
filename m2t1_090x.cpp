da3899e6b69ca7a300447ed32ef15a1ccf41bba1
moodye4980 <160150623+moodye4980@users.noreply.github.com>
2025-09-28 23:19:31 +0000

    M2T1 started :p

---
+++ m2t1_090x.cpp
@@ -0,0 +1,33 @@
+// CSC 134
+// M2T1 - Product Sales
+// Moody
+// 9/28/25
+// We're going to make the simplest possible
+// "checkout" machine.
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Set up all the variables
+    string first_name, last_name, full_name; // holds customer name 
+    string product = "apples"; // change to whatever you like
+    double cost_each = 0.99;
+    double total_cost;
+
+    // Greet the customer
+    cout << "Welcome to our " << product << "store!! <3" << endl;
+    cout << "What's your first name? ";
+    cin >> first_name;
+    cout << "What's your last name? ";
+    cin >> last_name;
+    full_name == first_name + " " + last_name;
+    cout << "Nice to meet you, " << full_name << endl;
+
+    // Ask how much they'd like to purchase
+
+    // Calculate total price
+    // Give the result
+
+    return 0; // no errors
+}
