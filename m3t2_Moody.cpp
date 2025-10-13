b99e206ad1f1347e4a09aa6bef370ce74ae3b011
moodye4980 <160150623+moodye4980@users.noreply.github.com>
2025-10-13 20:56:23 +0000

    first portion

---
+++ m3t2_Moody.cpp
@@ -0,0 +1,32 @@
+// CSC 134
+// M3T2
+// Emma Moody
+// 10/11/25
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // declare variables
+    int widthOne, lengthOne, widthTwo, lengthTwo;
+    int areaOne, areaTwo;
+
+    // ask for user input
+    cout << "Please enter the width and height of the first" << endl;
+    cout << "reactangle, seperated by a space or newline" << endl;
+    cin >> widthOne >> lengthOne;
+
+    cout << "Please enter the width and height of the second" << endl;
+    cout << "reactangle, seperated by a space or newline" << endl;
+    cin >> widthTwo >> lengthTwo;
+
+    // calculate answer
+    areaOne = widthOne * lengthOne;
+    areaTwo = widthTwo * lengthTwo;
+
+    // display results
+    cout << "Rectangle one has area of " << areaOne << endl;
+    cout << "Rectangle two has area of " << areaTwo << endl;
+    
